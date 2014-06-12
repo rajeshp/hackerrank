67d21a3e2de779add96a5bfd76aaf0f040210d82
U-ADOBENET\prajesh <prajesh@prajesh-W7-1.corp.adobe.com>
2014-06-12 18:07:50 +0530

    warmup problems

---
+++ src/GemStones.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+/**
+ * Created by prajesh on 6/12/2014.
+ */
+public class GemStones {
+
+    static boolean[][] compositionTable;
+
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+        compositionTable = new boolean[26][T];
+
+        String[] gems = new String[T];
+
+        for(int i=0;i<T;i++)
+        {
+            gems[i] = scanner.next();
+            addComposition(i,gems[i]);
+        }
+
+        System.out.println(getGemElementsCount(T));
+
+    }
+
+    private static int getGemElementsCount(int N)
+    {
+        int count=0;
+
+        for(int i=0;i<26;i++)
+        {
+            boolean b = true;
+
+            for(int j=0;j<N;j++)
+            {
+                b = b && compositionTable[i][j];
+            }
+
+            if(b)
+                count++;
+        }
+
+        return count;
+    }
+
+    private static void addComposition(int gemID, String gem)
+    {
+        if(gemID>=0 &&  gem!=null && gem.length() > 0) {
+            for (char c : gem.toCharArray()) {
+                compositionTable[(c - 97)][gemID] = true;
+            }
+        }
+    }
+
+}
---
+++ src/LoveLetterMystery.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+
+/**
+ * Created by prajesh on 6/12/2014.
+ */
+public class LoveLetterMystery {
+
+    public static void main(String[] args)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        int T = scanner.nextInt();
+
+
+        String[] originalWords = new String[T];
+        int[] changesRequired = new int[T];
+
+        for(int i=0;i<T;i++)
+        {
+            originalWords[i] = scanner.next();
+            changesRequired[i] = palindromeOperationsRequired(originalWords[i]);
+        }
+
+        //output
+        for(int i=0;i<T;i++)
+        {
+            System.out.println(changesRequired[i]) ;
+        }
+
+
+
+        //System.out.println(isPalindrome("ABCCBA"));
+
+
+    }
+
+
+
+    static int palindromeOperationsRequired(String word)
+    {
+        if(word==null || word.length()==0 || word.equals("") || isPalindrome(word)) return 0;
+
+
+
+        StringBuilder newWord = new StringBuilder(word);
+
+        int changesRequired=0;
+
+        int start=0, end = word.length()-1, mid=(start+end)/2;
+
+        while(start<=mid && end >=mid)
+        {
+
+            if(word.charAt(start)!= word.charAt(end))
+            {
+               changesRequired +=  Math.abs(word.charAt(start) - word.charAt(end));
+            }
+
+                start++;
+                end--;
+
+        }
+
+
+
+        return changesRequired;
+
+    }
+
+
+
+
+
+
+
+
+
+    static boolean isPalindrome(String word)
+    {
+        if(word==null|| word.equals("") || word.length()==0) return false;
+
+        int start=0, end = word.length()-1, mid=(start+end)/2;
+
+        while(start<=mid && end >=mid)
+        {
+
+            if(word.charAt(start)!= word.charAt(end))
+            {
+                break;
+            }
+            else
+            {
+                start++;
+                end--;
+            }
+
+        }
+
+        if(start>mid || end < mid)
+            return true;
+        else
+            return false;
+
+    }
+
+
+
+}
