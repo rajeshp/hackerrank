ca998d1e5d9b5ca23c8b7c47dba502d1ce5ebfe2
= <=>
2014-08-18 12:27:50 -0400

    Haloween party easy practice problem

---
+++ src/HaloweenParty.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+/**
+ * Created by prajesh on 18/08/14.
+ */
+public class HaloweenParty {
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int totalCases = scanner.nextInt();
+
+        long[] results = new long[totalCases];
+
+        for(int i=0;i<totalCases;i++)
+        {
+            results[i] = getMaxiumPieces(scanner.nextLong());
+        }
+
+        for(int i=0;i<totalCases;i++)
+        {
+            System.out.println(results[i]);
+        }
+
+    }
+
+
+    private static long getMaxiumPieces(long n)
+    {
+        if(n<=0) return 0;
+
+        if(n==1) return 1;
+
+
+            if(n%2==0)
+            {
+                long m = n/2 ;
+                return m * m ;
+            }
+            else
+            {
+                long m = n/2 ;
+                return m * (m+1) ;
+            }
+
+
+
+    }
+
+}
